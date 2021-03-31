dc06012721f070d454925322978d7ffddaff3ea4
Aditya Oberai <adityaoberai1@gmail.com>
2021-03-31 13:22:28 +0530

    Created Twilio Example Console App

---
+++ Twilio Stuff.cs
@@ -0,0 +1,7 @@
+﻿class Secrets
+{
+    public static string TWILIO_ACCOUNTSID { get; set; } = "ACe29c144db149972dbf5427bbdd0c16dd";
+    public static string TWILIO_AUTHTOKEN { get; set; } = "7f807277bee943eb005d00fbb0c1c3c4";
+    public static string TO_NUMBER { get; set; } = "+918439056262";
+    public static string FROM_NUMBER { get; set; } = "+12184232045";
+}
